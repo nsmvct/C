6f56808ab15c3f6eca6341f57f381b877c2e370a
nsmvct <nsmvct@gmail.com>
2026-06-01 17:28:04 -0300

    Create 03_system("pause").c

---
+++ 03_system("pause").c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    
+    // Prints the message to the screen
+    printf("Hello, World!\n");
+    
+    printf("\nPress Enter to continue... ");
+    
+    // Waits for the user to press Enter before closing
+    getchar(); 
+    
+    // Indicates the program finished successfully
+    return EXIT_SUCCESS; 
+}
