b5d11bbcdd88affd632e48733d9bb0f014af1666
nsmvct <nsmvct@gmail.com>
2026-06-01 17:08:59 -0300

    Create 02_printf.c

---
+++ 02_printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Print a simple message to the screen
+    printf("Hello, World!\n");
+    
+    // Print the text and then show how many characters it has
+    printf(" -> Total characters: %d\n", printf("hello world\n"));
+    
+    return 0;
+}
