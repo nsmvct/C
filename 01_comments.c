1df372dea31fa2ce108f5506138fba8bf07ac7b6
nsmvct <nsmvct@gmail.com>
2026-06-01 01:00:56 -0300

    fix: removendo arquivos temporarios

---
+++ 01_comments.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    // This is a line comment
+    
+    printf("hello world\n");
+    
+    /* This is a block comment 
+       that can span multiple lines */
+    return 0;
+}
+
